2bdeccb4f6e2c844be76af451e969d5ab70ed7c8
Sheikh Ahmed <sheikhishtiak@gmail.com>
2024-12-08 19:41:32 -0500

    Initial project setup using Spring Initializer with CRUD, Security, Caching, and Scheduling modules

---
+++ ProductManagerAPI/src/main/java/com/sheikhiahmed/ProductManagerAPI/ProductManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.sheikhiahmed.ProductManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApiApplication.class, args);
+	}
+
+}
---
+++ ProductManagerAPI/src/test/java/com/sheikhiahmed/ProductManagerAPI/ProductManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheikhiahmed.ProductManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
